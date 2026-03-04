9dd31ebfeb142c2e0acf465b6ada75ecf3de4bb6
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2026-03-04 12:49:25 -0700

    add menu component

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,90 @@
 export const COMPANY_NAME = "The Ember Room";
 export const PHONE = "(555) 123-4567";
+
+export const MENU_SECTIONS = [
+  {
+    title: "Pizzas",
+    items: [
+      {
+        name: "Margherita Verace",
+        description:
+          "San Marzano tomato, fior di latte, fresh basil, extra virgin olive oil",
+        price: "$18",
+      },
+      {
+        name: "Diavola",
+        description:
+          "Crushed tomato, smoked mozzarella, Calabrian chilli, fennel sausage, honey",
+        price: "$22",
+      },
+      {
+        name: "Funghi & Fontina",
+        description:
+          "Roasted wild mushrooms, fontina, thyme, garlic oil, toasted hazelnuts",
+        price: "$21",
+      },
+    ],
+  },
+  {
+    title: "Sides",
+    items: [
+      {
+        name: "Charred Broccolini",
+        description:
+          "Wood-roasted broccolini, chilli flakes, preserved lemon, anchovy crumb",
+        price: "$11",
+      },
+      {
+        name: "Focaccia al Rosmarino",
+        description:
+          "Oven-baked focaccia, rosemary, flaky sea salt, olive oil for dipping",
+        price: "$8",
+      },
+      {
+        name: "Arugula & Shaved Parm",
+        description:
+          "Wild rocket, 24-month Parmigiano Reggiano, lemon vinaigrette, cracked pepper",
+        price: "$12",
+      },
+    ],
+  },
+  {
+    title: "Soft Drinks",
+    items: [
+      {
+        name: "San Pellegrino Sparkling",
+        description: "250ml bottle, still or sparkling",
+        price: "$4",
+      },
+      {
+        name: "Blood Orange Soda",
+        description:
+          "House-made, fresh-squeezed blood orange, cane sugar, soda water",
+        price: "$6",
+      },
+      {
+        name: "Limonata",
+        description:
+          "Sicilian lemon juice, mint, sparkling water, a pinch of sea salt",
+        price: "$6",
+      },
+    ],
+  },
+  {
+    title: "Desserts",
+    items: [
+      {
+        name: "Nutella Calzone",
+        description:
+          "Folded pizza dough, Nutella, dusted with powdered sugar, served warm",
+        price: "$12",
+      },
+      {
+        name: "Affogato",
+        description:
+          "Double shot espresso poured over house-made vanilla bean gelato",
+        price: "$9",
+      },
+    ],
+  },
+];
